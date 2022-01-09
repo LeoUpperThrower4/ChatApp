6a8d6f7b103503dd1ba01257da7d8f27285b06e5
lduarte <lduarte@nelogica.com.br>
2022-01-09 18:07:12 -0300

    Read messages of Global chat from DB closes #6

---
+++ Utils/RealtimeDatabaseUtils.pas
@@ -0,0 +1,11 @@
+unit RealtimeDatabaseUtils;
+
+interface
+
+const
+  RealtimeDatabaseWebAPIKey = 'AIzaSyDUcS4IWiC7PVYH-5LT69gy--NJHmPZXs4';
+  RealtimeDatabaseURL       = 'https://chatapp-31972-default-rtdb.firebaseio.com/';
+
+implementation
+
+end.
